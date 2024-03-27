9f22f00d1f717cf8e740d0e4bedc3a1adea95e33
ibtisam-adil <ibtisamadil273@gmail.com>
2024-03-27 21:46:22 +0500

    Created Pokemon cards and popup

--- src/index.js
+++ src/index.js
@@ -1,2 +1,28 @@
 import './style.css';
-// import icon from './pokemon-log.png'
+// import fetchPokemonData from './modules/array.js';
+
+// class Pokemon {
+//   constructor() {
+//     const waterTypes = document.querySelector('.water-types');
+//   }
+
+//   fetchpokeData = () => (fetchPokemonData());
+// }
+
+// const pokemon = new Pokemon;
+
+const button = document.querySelector('.see-more');
+button.addEventListener('click', () => {
+  const blur = document.querySelector('#blur');
+  blur.classList.toggle('active');
+  const popup = document.querySelector('#popup');
+  popup.classList.toggle('active');
+});
+
+const cross = document.querySelector('#cross');
+cross.addEventListener('click', () => {
+  const blur = document.querySelector('#blur');
+  blur.classList.toggle('active');
+  const popup = document.querySelector('#popup');
+  popup.classList.toggle('active');
+});
